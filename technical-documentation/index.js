3a48bf991910439e61551b2ad5c012cf0f20473a
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-03-02 13:30:07 +0100

    display drop-down menus when dropdown-buttons are clicked

--- technical-documentation/index.js
+++ technical-documentation/index.js
@@ -4,14 +4,19 @@ document.addEventListener("click", (e) => {
   document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
     dropdown.classList.remove("active");
   });
+
+  // find the right dropdown based on what was clicked
+  const dropdownButton = e.target.matches("[data-dropdown-button")
+    ? e.target
+    : e.target.closest("[data-dropdown-button");
+
+  //if click happens outside of any dropdown-button
+  if (!dropdownButton) {
+    return;
+  }
+
+  //dropdown-button is clicked, display dropdown-menu
+  let currentDropDown = dropdownButton.querySelector("[data-dropdown");
+
+  currentDropDown.classList.add("active");
 });
-
-// find the right dropdown based on what was clicked
-const dropdownButton = e.target.matches("[data-dropdown-button")
-  ? e.target
-  : e.target.closest("[data-dropdown-button");
-
-//if click happens outside of any dropdown-button
-if (!dropdownButton) {
-  return;
-}
